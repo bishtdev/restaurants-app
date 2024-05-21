1ab903023e64f4c49bb7a94ee17778879f1ae3e8
bishtdev <bishtdev030@gmail.com>
2024-05-21 15:12:27 +0530

    setting up the project

---
+++ app/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import styled from 'styled-components'
+
+function App() {
+
+  return (
+  <MainContainer>
+    <TopContainer>
+      <div className='logo'>
+
+      </div>
+    </TopContainer>
+  </MainContainer>
+  )
+}
+
+export default App
+
+const MainContainer = styled.div``;
+const TopContainer = styled.div``
